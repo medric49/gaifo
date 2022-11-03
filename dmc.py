Fix reward computation

dmc.py
@@ -148,12 +148,13 @@ class RewardComputeStackWrapper(dm_env.Environment):
     def compute_obs_and_rewards(self):
         self.discriminator.eval()
         agent_obs = np.array(self.agent_obs, dtype=np.uint8)
+        agent_episode = agent_obs.copy()
         for t in range(self.nb_frame - 1):
-            agent_obs = np.insert(agent_obs, 0, agent_obs[0].copy(), axis=0)
-        agent_obs = VideoDataset.rgb_to_lab(agent_obs)[:, :, :, 0:1]
+            agent_episode = np.insert(agent_episode, 0, agent_episode[0].copy(), axis=0)
+        agent_episode = VideoDataset.rgb_to_lab(agent_episode)[:, :, :, 0:1]
         transitions = []
-        for t in range(0, agent_obs.shape[0] - (self.nb_frame - 1)):
-            transitions.append(np.concatenate([agent_obs[i] for i in range(t, t+self.nb_frame)], axis=2))
+        for t in range(0, agent_episode.shape[0] - (self.nb_frame - 1)):
+            transitions.append(np.concatenate([agent_episode[i] for i in range(t, t+self.nb_frame)], axis=2))
         transitions = np.array(transitions).transpose((0, 3, 1, 2))
         with torch.no_grad():
             transitions = torch.tensor(transitions, dtype=torch.float, device=utils.device())