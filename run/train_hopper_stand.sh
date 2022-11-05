8cec962a58a21f79c8c0221451d765bb17e54f96
Medric Sonwa <medric49@gmail.com>
2022-11-05 15:30:09 -0400

    update hopper stand

--- run/train_hopper_stand.sh
+++ run/train_hopper_stand.sh
@@ -1,4 +1,4 @@
 export PYTHONPATH="${PYTHONPATH}:`pwd`"
 
-# python3 scripts/generate_dmc_video.py --env hopper_stand --episode_len 60
+python3 scripts/generate_dmc_video.py --env hopper_stand --episode_len 60
 python3 train_gaifo.py task=hopper_stand
